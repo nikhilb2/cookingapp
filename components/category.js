58bc28f2d27466bda74851e8e0e6c035eb83e14b
Nikx <nikx@Kati4kas-MacBook-Pro.local>
2019-07-11 09:25:26 +0300

    category component

---
+++ components/category.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+const Category = props => {
+  const { category } = props
+  return(
+    <View>
+      <Text>Coming Soon</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+
+})
+
+export default Category
